d6842f695f6830f9de4a431dee500b9a4bda29f1
0xweb01 <ieee.ali404@gmail.com>
2023-10-05 01:50:12 +0300

    Update r10k.pp

--- site/profile/manifests/r10k.pp
+++ site/profile/manifests/r10k.pp
@@ -1,11 +1,11 @@
 class profile::r10k {
-  class {'r10k":
-     remote => 'https://github.com/0xweb01/control-repo/',
-  }
-  class {'r10k::webhook::config'
-     use_mcllective => false,
-     enable_ssl     => false,
-  }
+class {'r10k':
+   remote => 'https://github.com/0xweb01/control-repo/',
+}
+class {'r10k::webhook::config':
+   use_mcollective => false,
+   enable_ssl      => false,
+}
   class {'r10k::webhook':
     user   => 'root',
     group  => 'root',
